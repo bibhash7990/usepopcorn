05dd3111c03370c96b853d990124e30ee918931d
bibhash <bibhash88lenka@gmail.com>
2023-08-04 10:39:24 +0530

    initicommit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { useState } from "react";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
-import CodingChallenge from "./codingChallenge";
-import "./style.css";
+// import { useState } from "react";
+import "./index.css";
+import App from "./App";
+import App2 from "./challenge-geolocation";
+// import StarRating from "./StarRating";
+// import CodingChallenge from "./codingChallenge";
+// import "./style.css";
+// import CurrencyConverter from "./Currency-converter.js";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movies was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movies was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -28,6 +30,8 @@ root.render(
     />
     <StarRating size={24} color="red" className="test" defaultRating={2} />
     <Test /> */}
-    <CodingChallenge />
+    {/* <CodingChallenge /> */}
+    {/* <CurrencyConverter /> */}
+    <App2 />
   </React.StrictMode>
 );
---
+++ src/useMovies.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+
+const KEY = "5df11cde";
+
+// named export for custom hooks , not use the default export
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      //   callback?.();
+
+      const controller = new AbortController();
+
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok)
+            throw new Error("Sometthing went wrong with fetching movies");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found");
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          // console.error(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      //   handleCloseMovie();
+      fetchMovies();
+
+      //cleanup function
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+
+  return { movies, isLoading, error };
+}
